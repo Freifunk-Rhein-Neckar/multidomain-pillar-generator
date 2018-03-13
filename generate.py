c29a6fec8c4a081ad3bd7173986fff24a6c2f6a3
Martin Weinelt <hexa@darmstadt.ccc.de>
2018-03-13 19:05:38 +0100

    domain assignment updates

--- generate.py
+++ generate.py
@@ -20,24 +20,106 @@ header = 'Generated using https://git.darmstadt.ccc.de/ffda/multidomain-pillar-g
 
 # setup
 gateways = 8
-domain_names = OrderedDict([
-    ('default', 'Default'),
-    ('da-nord', 'Darmstadt Nord'),
-    ('da-sued', 'Darmstadt Süd'),
-    ('ah-kra-wix', 'Arheilgen, Kranichstein & Wixhauesn'),
-    ('weiterstadt', 'Weiterstadt'),
-    ('gg-nord', 'Groß-Gerau Nord'),
-    ('gg-sued', 'Groß-Gerau Süd'),
-    ('babenhausen', 'Babenhausen'),
-    ('muehltal', 'Mühltal'),
-    ('odenwald', 'Odenwald'),
-    ('da-di-nord', 'Darmstadt-Dieburg Nord'),
-    ('da-di-ost', 'Darmstadt-Dieburg Ost'),
-    ('da-di-sued', 'Darmstadt-Dieburg Süd'),
-    ('offenbach-sued', 'Offenbach Süd'),
-    ('bergstrasse-nord', 'Bergstraße Nord'),
-    ('bergstrasse-sued', 'Bergstraße Süd'),
-])
+
+domain_names = [
+    # dom0
+    {'ffda-default': "Default"},
+    # dom1
+    {'ffda-da-110': 'Darmstadt: Stadtzenrum',
+     'ffda-da-120': 'Darmstadt: Mollerstadt',
+     'ffda-da-130': 'Darmstadt: Hochschulviertel',
+     'ffda-da-210': 'Darmstadt: Johannesviertel',
+     'ffda-da-220-230': 'Darmstadt: Martinsviertel',
+     'ffda-da-270': 'Darmstadt: Bürgerparkviertel',
+     'ffda-da-310': 'Darmstadt: Am Oberfeld',
+     'ffda-da-320': 'Darmstadt: Mathildenhöhe'},
+    # dom2
+    {'ffda-da-240': 'Darmstadt: Waldkolonie',
+     'ffda-da-250': 'Darmstadt: Mornewegviertel',
+     'ffda-da-260': 'Darmstadt: Pallaswiesenviertel',
+     'ffda-da-530': 'Darmstadt: Verlegerviertel',
+     'ffda-da-540': 'Darmstadt: Am Kavalleriesand'},
+    # dom3
+    {'ffda-da-140': 'Darmstadt: Kapellplatzviertel',
+     'ffda-da-150': 'Darmstadt: St. Ludwig mit Eichbergviertel',
+     'ffda-da-330': 'Darmstadt: Woogsviertel',
+     'ffda-da-340': 'Darmstadt: An den Lichtwiesen',
+     'ffda-da-410': 'Darmstadt: Paulusviertel',
+     'ffda-da-420': 'Darmstadt: Alt-Bessungen'},
+    # dom4
+    {'ffda-da-430': 'Darmstadt: An der Luwdigshöhe',
+     'ffda-da-440': 'Darmstadt: Lincoln-Siedlung',
+     'ffda-da-510': 'Darmstadt: Am Südbahnhof',
+     'ffda-da-520': 'Darmstadt: Heimstättensiedlung'},
+    # dom5
+    {'ffda-da-610-620-630': 'Darmstadt-Arheilgen',
+     'ffda-da-910-920': 'Darmstadt-Kranichstein',
+     'ffda-da-810-820': 'Darmstadt-Wixhausen',
+     'ffda-64390': 'Erzhausen'},
+    # dom6
+    {'ffda-64521': 'Groß-Gerau',
+     'ffda-64546': 'Mörfelden-Walldorf',
+     'ffda-64572': 'Büttelborn',
+     'ffda-64569': 'Nauheim'},
+    # dom7
+    {'ffda-64579': 'Gernsheim',
+     'ffda-64560': 'Riedstadt',
+     'ffda-64589': 'Stockstadt am Rhein',
+     'ffda-64584': 'Biebesheim am Rhein'},
+    # dom8
+    {'ffda-64832': 'Babenhausen (Hessen)'},
+    # dom9
+    {'ffda-64347': 'Griesheim',
+     'ffda-64331': 'Weiterstadt'},
+    # dom10
+    {'ffda-64807': 'Dieburg',
+     'ffda-64409': 'Messel',
+     'ffda-64839': 'Münster (Hessen)',
+     'ffda-64859': 'Eppertshausen'},
+    # dom11
+    {'ffda-64846': 'Groß-Zimmern',
+     'ffda-64380': 'Roßdorf (bei Darmstadt)',
+     'ffda-64354': 'Reinheim',
+     'ffda-64401': 'Groß-Bieberau'},
+    # dom12
+    {'ffda-64853': 'Otzberg',
+     'ffda-64823': 'Groß-Umstadt',
+     'ffda-64850': 'Schaafheim'},
+    # dom13
+    {'ffda-64297': 'Darmstadt-Eberstadt',
+     'ffda-64342': 'Seeheim-Jugenheim',
+     'ffda-64665': 'Alsbach-Hähnlein',
+     'ffda-64319': 'Pfungstadt',
+     'ffda-64404': 'Bickenbach',
+     'ffda-64673': 'Zwingenberg'},
+    # dom14
+    {'ffda-64372': 'Ober-Ramstadt',
+     'ffda-64397': 'Modautal',
+     'ffda-64367': 'Mühltal',
+     'ffda-64405': 'Fischbachtal'},
+    # dom15
+    {'ffda-63303': 'Dreieich',
+     'ffda-63225': 'Langen',
+     'ffda-63329': 'Egelsbach'},
+    # dom16
+    {'ffda-63128': 'Dietzenbach',
+     'ffda-63110': 'Rodgau',
+     'ffda-63322': 'Rödermark',
+     'ffda-63500': 'Seligenstadt',
+     'ffda-63533': 'Mainhausen'},
+    # dom17
+    {'ffda-64732': 'Bad König',
+     'ffda-64747': 'Breuberg',
+     'ffda-64711': 'Erbach',
+     'ffda-64720': 'Michelstadt',
+     'ffda-64395': 'Brensbach',
+     'ffda-64753': 'Brombachtal',
+     'ffda-64407': 'Fränkisch-Crumbach',
+     'ffda-64739': 'Höchst im Odenwald',
+     'ffda-64750': 'Lützelbach',
+     'ffda-64756': 'Mossautal',
+     'ffda-64385': 'Reichelsheim (Odenwald)'},
+]
 
 # vpn
 fastd_port_range = range(10000, 10500)
@@ -47,7 +129,7 @@ fastd_peers_groups = ['nodes']
 mtu = 1312
 
 # layer 3
-prefix4_rfc1918 = '10.104.0.0/16'
+prefix4_rfc1918 = '10.84.0.0/15'
 prefix6_glob = '2001:67c:2ed8:1000::/56'
 prefix6_ula = 'fd01:67c:2ed8:1000::/56'
 prefixlen = 20
@@ -76,8 +158,13 @@ batadv_gw = {
 # begin domain template
 
 domain_template = jinja2.Template("""{
-	domain_names = { {{ domain }} = '{{ domain_name }}' },
-	domain_seed = '{{ domain_seed }}', -- sha256('ffda-dom{{ domain_id }}')
+	domain_names = {
+	    dom{{ domain_id }} = 'Domain {{ domain_id }}',
+	    {%- for domain_code, domain_name in domain_names.items() %}
+	    {{ domain_code }} = '{{ domain_name }}'{% if not loop.last %},{% endif %}
+	    {%- endfor %}
+	},
+	domain_seed = '{{ domain_seed }}',
 
 	prefix4 = '{{ prefix4 }}',
 	prefix6 = '{{ prefix6_ula }}',
@@ -215,7 +302,7 @@ gnt_network_cmds = []
 gnt_instance_cmds = []
 
 domains = {}
-for _id, domain in enumerate(domain_names.keys()):
+for _id, names in enumerate(domain_names):
     _ip6_global_prefix = ip6_prefixes_glob.__next__()
     _ip6_ula_prefix = ip6_prefixes_ula.__next__()
     prefix4_rfc1918 = ip4_prefixes.__next__()
@@ -226,14 +313,17 @@ for _id, domain in enumerate(domain_names.keys()):
     nextnode4 = prefix4_rfc1918[-2]
     nextnode6 = _ip6_ula_prefix[2**16+1]
 
-    gnt_network_cmds.append('gnt-network add --network={network} --mac-prefix=DA:FF:{id:02} {domain_name}\n'.format(network=prefix4_rfc1918, domain_name=domain, id=_id))
-    gnt_network_cmds.append('gnt-network connect --nic-parameters=mode=bridged,link=br-vlan{vid} {domain_name}\n\n'.format(vid=base_vid + _id, domain_name=domain))
+    gnt_network_cmds.append('gnt-network add --network={network} --mac-prefix=DA:FF:{id:02} dom{id}\n'.format(network=prefix4_rfc1918, id=_id))
+    gnt_network_cmds.append('gnt-network connect --nic-parameters=mode=bridged,link=br-vlan{vid} dom{id}\n\n'.format(vid=base_vid + _id, id=_id))
 
-    with open("gluon/domains/{}.conf".format(domain.replace('-', '_')), 'w') as gluon_site_handle:
+    with open("gluon/domains/dom{}.conf".format(_id), 'w') as gluon_site_handle:
         context = dict(
-            domain=domain.replace('-', '_'),
+            domain_names={
+                domain_code.replace('-', '_'): domain_name
+                for domain_code, domain_name
+                in names.items()
+            },
             domain_id=_id,
-            domain_name=domain_names[domain],
             domain_seed=sha256(bytes('ffda-dom{}'.format(_id), 'utf-8')).hexdigest(),
             prefix4=str(prefix4_rfc1918),
             prefix6_ula=str(_ip6_ula_prefix),
@@ -247,14 +337,13 @@ for _id, domain in enumerate(domain_names.keys()):
             domain_template.render(**context)
         )
 
-    with open('pillar/domains/{}_{}.sls'.format(_id, domain), 'w') as handle:
+    with open('pillar/domains/dom{}.sls'.format(_id), 'w') as handle:
         handle.write(yaml.dump({
             '__generator': header,
             'domains': {
-                domain: {
+                'dom{}'.format(_id): {
                     'domain_id': _id,
-                    'domain_name': domain,
-                    'domain_pretty': domain_names[domain],
+                    'domain_names': names,
                     'mtu': mtu,
                     'ip4': {
                         str(prefix4_rfc1918): {
@@ -289,7 +378,6 @@ for _id, domain in enumerate(domain_names.keys()):
                         ],
                         'domain': 'ffda.io',
                         'search': [
-                            '{}.ffda.io'.format(domain),
                             'ffda.io',
                             'darmstadt.freifunk.net'
                         ]
@@ -309,13 +397,13 @@ for _id, domain in enumerate(domain_names.keys()):
         }, default_flow_style=False))
 
     for i, pool in enumerate(dhcp_pools):
-        gnt_instance_cmds.append("gnt-instance modify --net add:network={},mac=da:ff:{:02d}:00:{:02d}:05 gw{:02d}\n".format(domain, _id, i+1, i+1))
+        gnt_instance_cmds.append("gnt-instance modify --net add:network=dom{},mac=da:ff:{:02d}:00:{:02d}:05 gw{:02d}\n".format(_id, _id, i+1, i+1))
 
-        with open('pillar/host/gw{:02d}/domains/{}_{}.sls'.format(i+1, _id, domain), 'w') as handle:
+        with open('pillar/host/gw{:02d}/domains/dom{}.sls'.format(i+1, _id), 'w') as handle:
             handle.write(yaml.dump({
                '__generator': header,
                'domains': {
-                    domain: {
+                    'dom{}'.format(_id): {
                         'ip4': {
                             str(prefix4_rfc1918): {
                                 'address': str(pool[1]),
@@ -357,8 +445,8 @@ with open('pillar/domains/init.sls', 'w') as handle:
     handle.write(yaml.dump({
         '__generator': header,
         'include': [
-            'domains.{}_{}'.format(domain_id, domain_name)
-            for domain_id, domain_name in enumerate(domain_names.keys())
+            'domains.dom{}'.format(domain_id)
+            for domain_id, _ in enumerate(domain_names)
         ]
     }, default_flow_style=False))
 
@@ -368,8 +456,8 @@ for i in range(gateways):
         handle.write(yaml.dump({
             '__generator': header,
             'include': [
-                'host.gw{:02d}.domains.{}_{}'.format(i+1, domain_id, domain_name)
-                for domain_id, domain_name in enumerate(domain_names.keys())
+                'host.gw{:02d}.domains.dom{}'.format(i+1, domain_id)
+                for domain_id, _ in enumerate(domain_names)
             ]
         }, default_flow_style=False))
 
@@ -378,6 +466,6 @@ with open("netbox_vlans.csv", "w") as fh:
     fh.write("# NetBox VLAN Import\n")
     fh.write("site,group_name,vid,name,tenant,status,role,description\n")
 
-    for domain_id, domain_name in enumerate(domain_names.keys()):
-        vid = base_vid + domain_id
-        fh.write("S2|02 C303,mesh-batadv,{},{},NOC,Active,Mesh (batman-adv),\"{}\"\n".format(vid, domain_name, domain_names[domain_name]))
+    for _id, names in enumerate(domain_names):
+        vid = base_vid + _id
+        fh.write("S2|02 C303,mesh-batadv,dom{},{},NOC,Active,Mesh (batman-adv),\"{}\"\n".format(vid, _id, ', '.join(names.values())))
